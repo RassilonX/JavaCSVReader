ce458f9b83ceab25517ed6da3cd2ba21b2d29511
RassilonX <chrisfraser18@gmail.com>
2024-08-22 20:01:25 +0100

    Added test data file path as hard coded string

--- CSVReader/CSVFileReader/src/Main.java
+++ CSVReader/CSVFileReader/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        String filePath = "Bibble";
+        String filePath = "C:\\Users\\RassilonX\\Desktop\\JavaCSVReader\\CustomerTestData.csv";
         System.out.println("Parsing CSV file located at: " + filePath);
         List<Customer> customers = CSVParser.ParseCSV(filePath);
 
