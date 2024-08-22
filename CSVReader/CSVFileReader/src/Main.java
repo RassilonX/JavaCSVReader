84ced80b418eb31dd88d35614e6fe443bd9dd03c
RassilonX <chrisfraser18@gmail.com>
2024-08-22 19:58:12 +0100

    Added the CSV parser to the main program flow

--- CSVReader/CSVFileReader/src/Main.java
+++ CSVReader/CSVFileReader/src/Main.java
@@ -1,11 +1,15 @@
+import java.io.IOException;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         String filePath = "Bibble";
         System.out.println("Parsing CSV file located at: " + filePath);
+        List<Customer> customers = CSVParser.ParseCSV(filePath);
 
         System.out.println("All data parsed Successfully");
     }
